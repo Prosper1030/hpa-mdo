81ee4ff507907cf1a1a1c617d93e18d89741f608
林禹安 <linyuan@linyuandeMac-mini.local>
2026-04-22 16:16:45 +0800

    fix: 收斂概念設計套件匯出介面

--- src/hpa_mdo/concept/__init__.py
+++ src/hpa_mdo/concept/__init__.py
@@ -1,21 +1,9 @@
 from hpa_mdo.concept.config import BirdmanConceptConfig, load_concept_config
-from hpa_mdo.concept.geometry import (
-    GeometryConcept,
-    WingStation,
-    build_linear_wing_stations,
-    build_segment_plan,
-    enumerate_geometry_concepts,
-)
 from hpa_mdo.concept.pipeline import ConceptPipelineResult, run_birdman_concept_pipeline
 
 __all__ = [
     "BirdmanConceptConfig",
-    "GeometryConcept",
     "ConceptPipelineResult",
-    "WingStation",
-    "build_linear_wing_stations",
-    "build_segment_plan",
-    "enumerate_geometry_concepts",
     "load_concept_config",
     "run_birdman_concept_pipeline",
 ]
